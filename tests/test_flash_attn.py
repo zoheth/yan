Fix calculation error, prepare for performance optimization

tests/test_flash_attn.py
@@ -1,8 +1,36 @@
 import torch
+from flash_attn import flash_attn_func
 
 import yan
-from yan import bench_kineto
+from yan import bench_kineto, calc_diff
+from yan.jit_kernels import flash_attn_cute
+
+def construct_run(num_heads: int, seq_len: int, head_dim: int):
+    Q = torch.randn(32, num_heads, seq_len, head_dim, device='cuda', dtype=torch.half)
+    K = torch.randn(32, num_heads, seq_len, head_dim, device='cuda', dtype=torch.half)
+    V = torch.randn(32, num_heads, seq_len, head_dim, device='cuda', dtype=torch.half)
+    Output = torch.zeros(32, num_heads, seq_len, head_dim, device='cuda', dtype=torch.half)
+    flash_attn_cute(Q, K, V, Output)
+    Q = Q.permute(0, 2, 1, 3)
+    K = K.permute(0, 2, 1, 3)
+    V = V.permute(0, 2, 1, 3)
+    expected_output = flash_attn_func(Q, K, V)
+    expected_output = expected_output.permute(0, 2, 1, 3)
+
+def test_flash_attn():
+    for seq_len in (1024, ):
+        for num_heads, head_dim in [(64, 128), ]:
+            def test_func():
+                construct_run(num_heads, seq_len, head_dim)
+                
+            kernel_names = ('flash_attn_cute', 'flash_fwd_kernel')
+            t = bench_kineto(test_func, kernel_names, suppress_kineto_output=True)
+            for i, time in enumerate(t):
+                print(f' > Performance (N={seq_len:5}, h={num_heads:5}, d={head_dim:5}) {kernel_names[i]}: {time * 1e6:4.0f} us')
+                
+            print()
 
 if __name__ == "__main__":
-    yan.jit_kernels.accuracy_test()
+    # yan.jit_kernels.accuracy_test()
+    test_flash_attn()
     

yan/jit_kernels/flash_attn.py
@@ -1,10 +1,12 @@
-import torch
 import math
 from typing import Tuple
 
+import torch
+from flash_attn import flash_attn_func
+
 from .tuner import jit_tuner
 
-includes = ('"flash_attn/flash_attn_naive.cuh"', )
+includes = ('"flash_attn/flash_attn.cuh"', )
 template = """
 // Templated args from Python JIT call
 constexpr auto d = {D};
@@ -12,7 +14,7 @@ constexpr auto d = {D};
 flash_attn_func<d>(Q, K, V, O, batch_size, num_heads, seq_len, stream);
 """
 
-def flash_attn(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, Output: torch.Tensor) -> None:
+def flash_attn_cute(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, Output: torch.Tensor) -> None:
     batch_size = Q.shape[0]
     num_heads = Q.shape[1]
     seq_len = Q.shape[2]
@@ -52,16 +54,21 @@ def accuracy_test():
         V = torch.randn(32, 64, 1024, 128, device='cuda', dtype=torch.half)
         Output = torch.zeros(32, 64, 1024, 128, device='cuda', dtype=torch.half)
         # Temp = torch.zeros(32, 64, 1024, 1024, device='cuda', dtype=torch.half)
-        flash_attn(Q, K, V, Output)
+        flash_attn_cute(Q, K, V, Output)
         
         # Calculate expected output: softmax(Q * K^T / sqrt(d)) * V
-        expected_output = torch.zeros_like(Output)
-        for b in range(32):
-            for h in range(64):
-                scale_factor = 1.0 / math.sqrt(Q.shape[-1])
-                temp = torch.matmul(Q[b, h], K[b, h].transpose(0, 1)) * scale_factor
-                temp = torch.softmax(temp, dim=-1)
-                expected_output[b, h] = torch.matmul(temp, V[b, h])
+        # expected_output = torch.zeros_like(Output)
+        # for b in range(32):
+        #     for h in range(64):
+        #         scale_factor = 1.0 / math.sqrt(Q.shape[-1])
+        #         temp = torch.matmul(Q[b, h], K[b, h].transpose(0, 1)) * scale_factor
+        #         temp = torch.softmax(temp, dim=-1)
+        #         expected_output[b, h] = torch.matmul(temp, V[b, h])
+        Q = Q.permute(0, 2, 1, 3)
+        K = K.permute(0, 2, 1, 3)
+        V = V.permute(0, 2, 1, 3)
+        expected_output = flash_attn_func(Q, K, V)
+        expected_output = expected_output.permute(0, 2, 1, 3)
                 
         # Calculate difference statistics
         diff = torch.abs(Output - expected_output)
@@ -86,7 +93,4 @@ def accuracy_test():
         print(f"Mean diff: {mean_diff:.6f}, Max diff: {max_diff:.6f}")
         print(f"Max diff at [{b},{h},{r},{c}]: {Output[b,h,r,c]:.6f} vs {expected_output[b,h,r,c]:.6f}")
         print(f"2nd max diff at [{b2},{h2},{r2},{c2}]: {Output[b2,h2,r2,c2]:.6f} vs {expected_output[b2,h2,r2,c2]:.6f}")
-        print(f"Verification: {'PASSED' if max_diff < 0.2 else 'FAILED'}")
-        
-        print(Output[18,18])
-        print(expected_output[18,18])
+        print(f"Verification: {'PASSED' if max_diff < 0.001 else 'FAILED'}")